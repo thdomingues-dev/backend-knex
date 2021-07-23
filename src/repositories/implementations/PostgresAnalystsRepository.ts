e1e8ff79387319238eeaeabae378f29e07768a95
Thales Domingues <thdomingues.dev@gmail.com>
2021-07-22 22:59:30 -0300

    feat: connect real database

--- src/repositories/implementations/PostgresAnalystsRepository.ts
+++ src/repositories/implementations/PostgresAnalystsRepository.ts
@@ -1,5 +1,6 @@
 import { Analyst } from "../../entities/Analyst";
 import { IAnalystsRepository } from "../IAnalystsRepository";
+import knex from '../../database'
 
 export class PostgresAnalystsRepository implements IAnalystsRepository {
 	private analysts: Analyst[] = []
@@ -8,6 +9,10 @@ export class PostgresAnalystsRepository implements IAnalystsRepository {
 		const analyst = this.analysts.find(analyst => analyst.email === email)
 
 		return analyst
+	}
+
+	async findAllAnalysts(): Promise<Analyst[]> {
+		return await knex('analysts')
 	}
 
 	async save(analyst: Analyst): Promise<void> {
