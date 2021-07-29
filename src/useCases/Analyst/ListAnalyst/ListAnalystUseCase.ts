7213efb2dc48156ec00793e035e2e321c6cfcf0a
Thales Domingues <thdomingues.dev@gmail.com>
2021-07-29 00:04:32 -0300

    refactor: improve folder organization

---
+++ src/useCases/Analyst/ListAnalyst/ListAnalystUseCase.ts
@@ -0,0 +1,13 @@
+import { Analyst } from "../../../entities/Analyst";
+import { IAnalystsRepository } from "../../../repositories/IAnalystsRepository";
+import { IListAnalystResponseDTO } from "./ListAnalystDTO";
+
+export class ListAnalystUseCase {
+	constructor(
+		private analystRepository: IAnalystsRepository
+	) {}
+
+	async execute(): Promise<Analyst[]> {
+		return await this.analystRepository.findAllAnalysts()
+	}
+}
