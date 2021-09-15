329e183bebb2e9f0bc4a7d1fd134092d10735a9b
Thales Domingues <thdomingues.dev@gmail.com>
2021-09-15 01:37:16 -0300

    chore: deploy heroku

--- knexfile.js
+++ knexfile.js
@@ -41,19 +41,14 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    client: 'pg',
+		connection: process.env.DATABASE_URL,
+		migrations: {
+			tableName: 'knex_migrations',
+			directory: `${__dirname}/src/database/migrations`
+		},
+		seeds: {
+			directory: `${__dirname}/src/database/seeds`
+		}
   }
-
 };
