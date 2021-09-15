611f299259ae6ee90a16d7935b5272ac50ccc17c
Thales Domingues <thdomingues.dev@gmail.com>
2021-09-15 02:29:40 -0300

    fix: heroku ts serror

--- src/server.ts
+++ src/server.ts
@@ -15,14 +15,14 @@ app.use(express.json())
 app.use(routes)
 
 //not found
-app.use((_req: any,_res: any, next: (arg0: Error) => void) => {
+app.use((_req,_res, next) => {
 	const error: any = new Error('Not Found')
 	error.status = 404
 	next(error)
 })
 
 //catch all
-app.use((error: any, _req: any, res: { status: (arg0: any) => void; json: (arg0: { error: any }) => void }, _next: any) => {
+app.use((error, _req, res, _next) => {
 	res.status(error.status || 500)
 	res.json({ error: error.message})
 })
